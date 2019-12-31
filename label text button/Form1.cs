6b48f18444cc6a878d09e981284f71b4c41024b8
Mert Can Sulupinar <58821990+mertcansulupinar@users.noreply.github.com>
2019-12-31 23:32:23 +0300

    Update Form1.cs

--- label text button/Form1.cs
+++ label text button/Form1.cs
@@ -20,7 +20,10 @@ namespace label_text_button
         private void Button1_Click(object sender, EventArgs e)
         {
             label1.Text = textBox1.Text.ToString();
-            label1.Visible = true;
+            label1.Visible = true; /*Metin kısmımızı uygulama açıldığında görünmesini istemiyorsak
+                                     bunu formumuzun design kısmından gizliyoruz ardından butona
+                                     basıldığında görünürlüğünü aktif hale getiriyoruz ki metin
+                                     kutusuna yazdıklarımız ekranda görünür hale gelebilsin*/ 
         }
 
        
