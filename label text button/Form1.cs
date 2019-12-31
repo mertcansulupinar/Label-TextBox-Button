8373a4f3ae93ce2b7dc16d636e30f9a50797c153
Mert Can Sulupinar <mertcansulupinar@gmail.com>
2019-12-31 22:42:43 +0300

    Add project files.

---
+++ label text button/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace label_text_button
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text.ToString();
+            label1.Visible = true;
+        }
+
+       
+    }
+}
